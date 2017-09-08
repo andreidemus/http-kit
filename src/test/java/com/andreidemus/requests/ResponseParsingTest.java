e115edb607e0e45ea0c2fe11ff544ba7420ef572
Andrei Demus <andrei.demus@gmail.com>
2017-09-08 23:43:07 +0300

    Add test for response headers

--- src/test/java/com/andreidemus/requests/ResponseParsingTest.java
+++ src/test/java/com/andreidemus/requests/ResponseParsingTest.java
@@ -5,13 +5,12 @@ import org.glassfish.grizzly.http.util.HttpStatus;
 import org.junit.Test;
 
 import static com.xebialabs.restito.builder.stub.StubHttp.whenHttp;
-import static com.xebialabs.restito.semantics.Action.ok;
-import static com.xebialabs.restito.semantics.Action.status;
-import static com.xebialabs.restito.semantics.Action.stringContent;
+import static com.xebialabs.restito.semantics.Action.*;
 import static com.xebialabs.restito.semantics.Condition.method;
 import static com.xebialabs.restito.semantics.Condition.uri;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.*;
 
 public class ResponseParsingTest extends RequestsTest {
     @Test
@@ -42,5 +41,23 @@ public class ResponseParsingTest extends RequestsTest {
         assertThat(resp.status(), is(401));
         assertThat(resp.hasBody(), is(true));
         assertThat(resp.text(), is("error body"));
+    }
+
+    @Test
+    public void headersParsedCorrectly() throws Exception {
+        final String path = "/headers-parsed-correctly";
+
+        whenHttp(server).match(
+                method(Method.GET),
+                uri(path)
+        ).then(
+                ok(),
+                header("Header1", "value1"),
+                header("Header2", "value2")
+        );
+
+        final Response resp = Requests.get(getUrl(path));
+        assertThat(resp.header("Header1"), contains("value1"));
+        assertThat(resp.header("header2"), contains("value2"));
     }
 }
